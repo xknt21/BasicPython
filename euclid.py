185d657ca3053d8130745a2d68b6f0ff07724cb2
xknt21 <kanato.n.0821@gmail.com>
2024-03-17 23:42:37 +0900

    制御構文_課題解答

--- euclid.py
+++ euclid.py
@@ -1,4 +1,18 @@
-a = input("a の値を入力: ")
-b = input("b の値を入力: ")
+a = input("a の値を入力")
+b = input("b の値を入力")
 
 # TODO
+a = int(a)
+b = int(b)
+def euclid(a, b):
+    if a < b:
+        a, b = b, a
+
+    remain = a % b
+
+    if remain == 0:
+        return b
+    else:
+        return euclid(b, remain)
+
+print(euclid(a, b)) 
--- machine_epsilon.py
+++ machine_epsilon.py
@@ -1,1 +1,8 @@
 # TODO
+e = 1
+
+while 1 + e > 1:
+    e /= 2
+
+print(e * 2)
+
